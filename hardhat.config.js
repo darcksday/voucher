2fba30aa12d7a780da0017b4fe41d55b46fe81d3
MaksP <darcksday@gmail.com>
2022-08-20 16:58:54 +0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
+  console.log(process.env.V_MUMBAI_RPC)
 
   for (const account of accounts) {
     console.log(account.address);
@@ -21,8 +21,7 @@ module.exports = {
   solidity: "0.8.12",
   networks: {
 
-    localhost: {
-    },
+    localhost: {},
 
     mumbai: {
       url: process.env.V_MUMBAI_RPC,
