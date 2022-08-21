4880e74e46f2a5063cfe37053ee6175d41fa590b
MaksP <darcksday@gmail.com>
2022-08-21 15:30:09 +0300

    update

--- src/context/RequestContext.jsx
+++ src/context/RequestContext.jsx
@@ -7,7 +7,6 @@ import ERC20ContractAbi from '@openzeppelin/contracts/build/contracts/ERC20.json
 import { useWeb3React } from '@web3-react/core';
 
 export const RequestContext = React.createContext();
-console.log(123)
 const [USDC_CONTRACT] = [process.env.USDC_CONTRACT];
 
 
